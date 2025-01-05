208ee056b2046dfc41ae4053551eea401a930f0b
kshitijkota <59368236+kshitijkota@users.noreply.github.com>
2025-01-05 19:44:40 +0530

    Add files via upload

---
+++ Longest_substing_without_repeating_chars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int lengthOfLongestSubstring(char* s) {
+    int longest;
+    char array[26];
+    for(int i=0;i<strlen(s);i++){
+
+        for(int j=0;j<strlen(s);j++){
+
+        }
+    }
+}
